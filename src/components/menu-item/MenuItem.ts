5a1fb7cf7cfbabd1dc905b10d9f1c4fcdbf4a058
leifriksheim <riksheim.leif@gmail.com>
2021-05-20 15:59:53 +0200

    sizes on menuitem

--- src/components/menu-item/MenuItem.ts
+++ src/components/menu-item/MenuItem.ts
@@ -4,6 +4,7 @@ import sharedStyles from "../../shared/styles";
 
 const styles = css`
   :host {
+    --j-menu-item-height: var(--j-element-md);
     --j-menu-item-bg: transparent;
     --j-menu-item-bg-hover: var(--j-color-ui-50);
   }
@@ -11,32 +12,38 @@ const styles = css`
     --j-menu-item-bg: var(--j-color-primary-300);
     --j-menu-item-bg-hover: var(--j-color-primary-300);
   }
+  :host([size="sm"]) {
+    --j-menu-item-height: var(--j-element-sm);
+  }
+  :host([size="lg"]) {
+    --j-menu-item-height: var(--j-element-lg);
+  }
+  :host([size="xl"]) {
+    --j-menu-item-height: var(--j-element-xl);
+  }
   :host(:last-of-type) [part="base"] {
     margin-bottom: 0;
   }
   [part="base"] {
     display: flex;
+    align-items: center;
     gap: var(--j-space-400);
     border-radius: var(--j-border-radius);
     background: var(--j-menu-item-bg);
     text-decoration: none;
     cursor: pointer;
     width: 100%;
-    padding: var(--j-space-400) var(--j-space-500);
+    height: var(--j-menu-item-height);
+    padding: 0 var(--j-space-500);
+    color: var(--j-color-ui-500);
+    font-weight: 400;
   }
   [part="base"]:hover {
+    color: var(--j-color-ui-800);
     background: var(--j-menu-item-bg-hover);
   }
   [part="content"] {
     flex: 1;
-  }
-  [part="title"] {
-    font-size: var(--j-font-size-500);
-    color: var(--j-color-ui-800);
-  }
-  [part="description"] {
-    font-size: var(--j-font-size-400);
-    color: var(--j-color-ui-500);
   }
   [part="start"]::slotted(*) {
     margin-left: var(--j-space-100);
@@ -51,22 +58,6 @@ export default class MenuItem extends LitElement {
   static styles = [sharedStyles, styles];
 
   /**
-   * Title
-   * @type {String}
-   * @attr
-   */
-  @property({ type: String, reflect: true })
-  title = "";
-
-  /**
-   * Description
-   * @type {String}
-   * @attr
-   */
-  @property({ type: String, reflect: true })
-  description = "";
-
-  /**
    * Selected
    * @type {Boolean}
    * @attr
@@ -78,8 +69,7 @@ export default class MenuItem extends LitElement {
     return html`<div part="base" role="menuitem">
       <slot name="start"></slot>
       <div part="content">
-        <div part="title">${this.title}</div>
-        <div part="description">${this.description}</div>
+        <slot></slot>
       </div>
       <slot name="end"></slot>
     </div>`;
