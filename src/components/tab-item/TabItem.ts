e6631d5961c1da4bde7db0a8a03e0c7457a59beb
leifriksheim <riksheim.leif@gmail.com>
2021-05-25 17:48:00 +0200

    feature: add tabs

---
+++ src/components/tab-item/TabItem.ts
@@ -0,0 +1,102 @@
+import { LitElement, html, css } from "lit";
+import { state, customElement, property } from "lit/decorators.js";
+import sharedStyles from "../../shared/styles";
+
+const styles = css`
+  :host {
+    z-index: 1;
+  }
+  [part="base"] {
+    outline: 0;
+    font-family: var(--j-font-family);
+    color: var(--j-color-ui-600);
+    cursor: pointer;
+    z-index: 1;
+    border: 0;
+    font-weight: 500;
+    font-size: var(--j-font-size-500);
+    background: 0;
+    padding: 0 var(--j-space-500);
+  }
+  [part="base"]:hover {
+    color: var(--j-color-primary-500);
+  }
+  :host([checked]) [part="base"] {
+    color: var(--j-color-primary-500);
+  }
+`;
+
+@customElement("j-tab-item")
+class TabItem extends LitElement {
+  @property({ type: Boolean, reflect: true })
+  checked = false;
+
+  @property({ type: Boolean, reflect: true })
+  full = false;
+
+  @property({ type: Boolean, reflect: true })
+  disabled = false;
+
+  @property({ type: String, reflect: true })
+  size = "";
+
+  @state()
+  _label = "";
+
+  @state()
+  _value = "";
+
+  get label() {
+    return (
+      this._label ||
+      this.getAttribute("label") ||
+      this.innerText ||
+      this.innerHTML
+    );
+  }
+
+  set label(val) {
+    this._label = val;
+    this.setAttribute("label", val);
+  }
+
+  get value() {
+    return (
+      this._value ||
+      this.getAttribute("value") ||
+      this.innerText ||
+      this.innerHTML
+    );
+  }
+
+  set value(val) {
+    this._value = val;
+    this.setAttribute("value", val);
+  }
+
+  static get styles() {
+    return [styles, sharedStyles];
+  }
+
+  _handleChange(e) {
+    e.stopPropagation();
+    this.checked = true;
+    this.dispatchEvent(new CustomEvent("tab-selected", { bubbles: true }));
+  }
+
+  render() {
+    return html`
+      <button
+        aria-selected=${this.checked}
+        aria-controls=${this.value}
+        @click=${this._handleChange}
+        part="base"
+        role="tab"
+      >
+        <slot></slot>
+      </button>
+    `;
+  }
+}
+
+export default TabItem;
---
+++ src/components/tabs/Tabs.ts
@@ -0,0 +1,110 @@
+import { html, css, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import sharedStyles from "../../shared/styles";
+
+const styles = css`
+  :host {
+    --j-tabs-padding: 4px;
+    --j-tabs-height: var(--j-element-md);
+    --j-tabs-indicator-width: 0px;
+    --j-tabs-indicator-left: 0px;
+  }
+  :host {
+    position: relative;
+  }
+  [part="base"] {
+    height: var(--j-tabs-height);
+    display: inline-flex;
+    align-items: center;
+    justify-content: space-around;
+    gap: var(--j-space-200);
+    background: var(--j-color-white);
+    border-radius: var(--j-border-radius);
+    border: 1px solid var(--j-color-ui-50);
+    padding: 0 var(--j-tabs-padding);
+    position: relative;
+  }
+  [part="base"]:before {
+    display: block;
+    content: "";
+    z-index: 0;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: var(--j-tabs-indicator-left);
+    width: var(--j-tabs-indicator-width);
+    height: calc(100% - calc(var(--j-tabs-padding) * 2));
+    background: var(--j-color-primary-100);
+    border-radius: var(--j-border-radius);
+    transition: all 0.2s ease;
+  }
+  :host([size="lg"]) {
+    --j-tabs-height: var(--j-element-lg);
+  }
+`;
+
+@customElement("j-tabs")
+export default class Menu extends LitElement {
+  static styles = [sharedStyles, styles];
+
+  @property({ type: String, reflect: true })
+  value = "";
+
+  @property({ type: String, reflect: true })
+  size = "";
+
+  get optionElements() {
+    return [...this.children];
+  }
+
+  get selectedElement() {
+    return this.optionElements.find(
+      (opt: any) => opt.value === this.value
+    ) as any;
+  }
+
+  shouldUpdate(changedProperties) {
+    if (changedProperties.has("value")) {
+      this.dispatchEvent(new CustomEvent("change", { bubbles: true }));
+    }
+    return true;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    if (this.value) {
+      setTimeout(() => {
+        this.style.setProperty(
+          "--j-tabs-indicator-width",
+          this.selectedElement?.offsetWidth + "px"
+        );
+        this.style.setProperty(
+          "--j-tabs-indicator-left",
+          this.selectedElement?.offsetLeft + "px"
+        );
+      }, 100);
+    }
+
+    this.addEventListener("tab-selected", (e: any) => {
+      e.stopPropagation();
+      this.optionElements.forEach((el: any) => {
+        const isChecked = el.value === e.target.value;
+        el.checked = isChecked;
+        this.value = e.target.value;
+        this.style.setProperty(
+          "--j-tabs-indicator-width",
+          e.target.offsetWidth + "px"
+        );
+        this.style.setProperty(
+          "--j-tabs-indicator-left",
+          e.target.offsetLeft + "px"
+        );
+      });
+    });
+  }
+
+  render() {
+    return html` <div part="base" role="tablist"><slot></slot></div>`;
+  }
+}
