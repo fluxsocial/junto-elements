2906a3a3d8e66701339ec72fd506f0acb300c6bb
Leif Riksheim <riksheim.leif@gmail.com>
2021-07-12 16:05:10 +0200

    chore: update component template

---
+++ src/components/_template/Component.docs.js
@@ -0,0 +1,8 @@
+import { html } from "https://unpkg.com/htm/preact/standalone.module.js";
+
+export default {
+  name: "Component",
+  description: "Component",
+  tag: "j-component",
+  component: () => html`<j-component>Component</j-component>`,
+};
