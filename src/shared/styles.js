0f797e67623d3d909c02a8cbf0b0a8123e2b9f8d
Leif Riksheim <riksheim.leif@gmail.com>
2021-08-11 14:52:54 +0200

    chore: add scrollbar variables to all components

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -18,4 +18,26 @@ export default css`
   [hidden] {
     display: none !important;
   }
+
+  ::-webkit-scrollbar {
+    width: var(--j-scrollbar-width, 10px);
+  }
+
+  ::-webkit-scrollbar-track {
+    background-image: var(--j-scrollbar-background-image, none);
+    background: var(--j-scrollbar-background, transparent);
+  }
+
+  ::-webkit-scrollbar-corner {
+    background: var(--j-scrollbar-corner-background, #dfdfdf);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    box-shadow: var(--j-scrollbar-thumb-box-shadow, none);
+    border-radius: var(--j-scrollbar-thumb-border-radius, 300px);
+    background-color: var(
+      --j-scrollbar-thumb-background,
+      rgba(180, 180, 180, 0.5)
+    );
+  }
 `;
